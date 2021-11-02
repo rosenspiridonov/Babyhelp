5f728f0250a38f62235b99064fce1a6ccd15d31d
Rosen Spiridonov <rspiridonov3@gmail.com>
2021-11-02 22:50:07 +0200

    Restructure Startup class by adding extention methods

---
+++ src/Homeohelp.Server/Infrastructure/ConfigurationExtentions.cs
@@ -0,0 +1,25 @@
+﻿namespace Homeohelp.Server.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ConfigurationExtentions
+    {
+        public static string GetDefaultConnectionString(this IConfiguration configuration)
+            => configuration.GetConnectionString("DefaultConnection");
+
+        public static AppSettings GetAppSettings(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var appSettingsSection = configuration.GetSection("ApplicationSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+            return appSettingsSection.Get<AppSettings>();
+        }
+    }
+}
--- src/Homeohelp.Server/Startup.cs
+++ src/Homeohelp.Server/Startup.cs
@@ -1,21 +1,14 @@
 namespace Homeohelp.Server
 {
-    using System.Text;
-
     using Data;
+    using Infrastructure;
 
-    using Homeohelp.Server.Data.Models;
-    using Homeohelp.Server.Infrastructure;
-
-    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.IdentityModel.Tokens;
 
     public class Startup
     {
@@ -25,51 +18,13 @@ namespace Homeohelp.Server
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
-        {
-            services
+            => services
                 .AddDbContext<ApplicationDbContext>(options => options
-                .UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services
-                .AddIdentity<User, IdentityRole>(options =>
-                {
-                    options.Password.RequiredLength = 6;
-                    options.Password.RequireDigit = false;
-                    options.Password.RequireNonAlphanumeric = false;
-                    options.Password.RequireUppercase = false;
-                    options.Password.RequireLowercase = false;
-                })
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            var appSettingsSection = this.Configuration.GetSection("ApplicationSettings");
-            services.Configure<AppSettings>(appSettingsSection);
-
-            var appSettings = appSettingsSection.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-            services
-                .AddAuthentication(x =>
-                {
-                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(x =>
-                {
-                    x.RequireHttpsMetadata = false;
-                    x.SaveToken = false;
-                    x.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(key),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                }); 
-
-            services.AddControllers();
-        }
+                    .UseSqlServer(this.Configuration.GetDefaultConnectionString()))
+                .AddIdentity()
+                .AddJwtAuthentication(services.GetAppSettings(this.Configuration))
+                .AddDatabaseDeveloperPageExceptionFilter()
+                .AddControllers();
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -78,22 +33,19 @@ namespace Homeohelp.Server
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseCors(options => options
-                .AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod());
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-
-            app.ApplyMigrations();
+            app
+                .UseRouting()
+                .UseCors(options => options
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod())
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                })
+                .ApplyMigrations();
         }
     }
 }
